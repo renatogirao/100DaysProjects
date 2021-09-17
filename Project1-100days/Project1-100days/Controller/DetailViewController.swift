6dc02c44e255ac9b8e80e06be2eda31263a6e73c
Renato Girão <renato.sgirao@gmail.com>
2021-09-17 02:56:25 -0300

    first commit, uploading first project

---
+++ Project1-100days/Project1-100days/Controller/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Project1-100days
+//
+//  Created by Renato Savoia Girão on 16/09/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = selectedImage
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
+
