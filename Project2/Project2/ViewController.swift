13cc4d17308c0549b9970a9bb5a3362e776312cd
Renato Girão <renato.sgirao@gmail.com>
2021-09-17 03:12:07 -0300

    first commit of project 2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Renato Savoia Girão on 17/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
